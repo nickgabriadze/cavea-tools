c72c44bf1e345122fff554f75e1f2f025ad23fc5
Nika Gabriadze <nickgabriadze@gmail.com>
2023-05-06 12:54:54 +0400

    Connected the database

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,12 +1,27 @@
 import express from "express";
 import cors from "cors";
-
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 export const server = express();
-server.use(cors())
-server.use(express.json())
 
+server.use(cors());
+server.use(express.json());
+dotenv.config();
+const sequalized = new Sequelize("cavetools", "postgres", process.env.DB_PASSWORD, {
+    host: "localhost",
+    dialect: 'postgres'
+});
 
+const testDbConnection = async () => {
+  try {
+    await sequalized.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.log(error)
+  }
+};
 
+testDbConnection();
 server.listen(3001, () => {
-    console.log("up and running")
-})
+  console.log("up and running");
+});
