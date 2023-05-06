e0c8aa36727e2159ea009906e13405835ea8ddbf
Nika Gabriadze <nickgabriadze@gmail.com>
2023-05-06 11:19:49 +0400

    Added button that opens /add page

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,22 @@
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import appStyles from "./app.module.css";
+import { Link } from "react-router-dom";
 
-const App = () => {
+export const App = () => {
+  return (
+    <div className={appStyles["items"]}>
+      <Link to={"/add"}>
+        <button
+          type="button"
+          className="btn btn-primary btn-sm"
+          aria-label="Submit"
+        >
+          დაამატეთ ახალი ნივთი
+        </button>
+      </Link>
+      
+    </div>
+  );
+};
 
-
-  return (<h1>Hi</h1>)
-}
-
-export default App
+export default App;
