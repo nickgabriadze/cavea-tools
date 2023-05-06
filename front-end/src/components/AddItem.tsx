ee27fcca430219490d0a9dd20ce60598a1641f48
Nika Gabriadze <nickgabriadze@gmail.com>
2023-05-06 11:13:07 +0400

    Fixed a few issues

--- front-end/src/components/AddItem.tsx
+++ front-end/src/components/AddItem.tsx
@@ -16,19 +16,21 @@ export const AddItem = () => {
   });
 
   const [popUp, setPopUp] = useState<boolean>(false);
-  const [requestInfo, setRequestInfo] = useState<string>("")
-
+  const [requestInfo, setRequestInfo] = useState<string>("");
 
   const PopUpBox = ({ message }: { message: string }) => {
     return (
       <div className={addItemStyles["pop-up"]}>
         <img
-        className={addItemStyles["tools-icon"]}
-        src={"/public/tool-icon.svg"}
-      />
+          className={addItemStyles["tools-icon"]}
+          src={"/public/tool-icon.svg"}
+        />
         <p>{message}</p>
-        <button type="button" className="btn btn-secondary" aria-label="Close"
-        onClick={() => setPopUp(prev => !prev)}
+        <button
+          type="button"
+          className="btn btn-secondary"
+          aria-label="Close"
+          onClick={() => setPopUp((prev) => !prev)}
         >
           დახურვა
         </button>
@@ -39,94 +41,106 @@ export const AddItem = () => {
   const checker = () => {
     if (
       itemInfo.name.trim().length === 0 ||
-      itemInfo.price === -1
+      itemInfo.price === -1 ||
+      itemInfo.price.toString() === ""
     ) {
-        setRequestInfo("არც ერთი ველი არ უნდა იყოს ცარიელი")
-    }else{
-        setRequestInfo("ნივთი დაემატა წარმატებით!")
+      setRequestInfo("არც ერთი ველი არ უნდა იყოს ცარიელი");
+    } else {
+      setRequestInfo("ნივთი დაემატა წარმატებით!");
     }
 
-    setPopUp(true)
+    setPopUp(true);
   };
 
   const blurStyle = {
-    "filter": "blur(5px)"
-  }
+    filter: "blur(5px)",
+  };
   return (
     <>
-    <section className={addItemStyles["for-new-item"]} style={popUp ? blurStyle : {}} >
-      <img
-        className={addItemStyles["tools-icon"]}
-        src={"/public/tool-icon.svg"}
-      />
-      <h5 className="fs-5">დაამატეთ სასურველი ნივთი</h5>
-      <div className="d-flex flex-column gap-2">
-        <div className="input-group mb-1">
-          <label className="input-group-text">ადგილმდებარეობა</label>
-          <select
-            className="form-select"
-            id="inputGroupSelect01"
-            onChange={(e) =>
-              setItemInfo({
-                ...itemInfo,
-                place: e.target.value,
-              })
-            }
+      <div className={addItemStyles["box-wrapper"]}>
+        <section
+          className={addItemStyles["for-new-item"]}
+          style={popUp ? blurStyle : {}}
+        >
+          <img
+            className={addItemStyles["tools-icon"]}
+            src={"/public/tool-icon.svg"}
+          />
+          <h5 className="fs-5">დაამატეთ სასურველი ნივთი</h5>
+          <div className="d-flex flex-column gap-2">
+            <div className="input-group mb-1">
+              <label className="input-group-text">ადგილმდებარეობა</label>
+              <select
+                className="form-select"
+                id="inputGroupSelect01"
+                onChange={(e) =>
+                  setItemInfo({
+                    ...itemInfo,
+                    place: e.target.value,
+                  })
+                }
+              >
+                <option value="მთავარი ოფისი">მთავარი ოფისი</option>
+                <option value="კავეა გალერია">კავეა გალერია</option>
+                <option value="კავეა თბილისი მოლი">კავეა თბილისი მოლი</option>
+                <option value="კავეა სითი მოლი">კავეა სითი მოლი</option>
+                <option value="კავეა ისთ ფოინთი">კავეა ისთ ფოინთი</option>
+              </select>
+            </div>
+
+            <div className="form-floating">
+              <input
+                type="text"
+                className="form-control h-50"
+                id="floatingInput"
+                placeholder="მაგ: ნივთ 1"
+                defaultValue={itemInfo.name}
+                onChange={(e) =>
+                  setItemInfo({
+                    ...itemInfo,
+                    name: e.target.value,
+                  })
+                }
+              />
+              <label>ნივთის სახელი</label>
+            </div>
+
+            <div className="form-floating ">
+              <input
+                type="number"
+                min={0}
+                className="form-control h-50"
+                id="floatingPassword"
+                placeholder="მაგ: 20"
+                defaultValue={0}
+                onChange={(e) => {
+                  if (e.target.value === "") {
+                    setItemInfo({
+                      ...itemInfo,
+                      price: -1,
+                    });
+                  } else {
+                    setItemInfo({
+                      ...itemInfo,
+                      price: Number(e.target.value),
+                    });
+                  }
+                }}
+              />
+              <label>ნივთის ფასი(ლარებში)</label>
+            </div>
+          </div>
+          <button
+            type="button"
+            className="btn btn-primary"
+            aria-label="Submit"
+            onClick={() => checker()}
           >
-            <option value="მთავარი ოფისი">მთავარი ოფისი</option>
-            <option value="კავეა გალერია">კავეა გალერია</option>
-            <option value="კავეა თბილისი მოლი">კავეა თბილისი მოლი</option>
-            <option value="კავეა სითი მოლი">კავეა სითი მოლი</option>
-            <option value="კავეა ისთ ფოინთი">კავეა ისთ ფოინთი</option>
-          </select>
-        </div>
-
-        <div className="form-floating">
-          <input
-            type="text"
-            className="form-control h-50"
-            id="floatingInput"
-            placeholder="მაგ: ნივთ 1"
-            defaultValue={itemInfo.name}
-            onChange={(e) =>
-              setItemInfo({
-                ...itemInfo,
-                name: e.target.value,
-              })
-            }
-          />
-          <label>ნივთის სახელი</label>
-        </div>
-
-        <div className="form-floating ">
-          <input
-            type="number"
-            className="form-control h-50"
-            id="floatingPassword"
-            placeholder="მაგ: 20"
-            defaultValue={0}
-            onChange={(e) =>
-              setItemInfo({
-                ...itemInfo,
-                price: Number(e.target.value),
-              })
-            }
-          />
-          <label>ნივთის ფასი(ლარებში)</label>
-        </div>
+            დამატება
+          </button>
+        </section>
       </div>
-      <button
-        type="button"
-        className="btn btn-primary"
-        aria-label="Submit"
-        onClick={() => checker()}
-      >
-        დამატება
-      </button>
-
-      
-    </section>
-    {popUp && <PopUpBox message={requestInfo} />}
+      {popUp && <PopUpBox message={requestInfo} />}
     </>
   );
 };
